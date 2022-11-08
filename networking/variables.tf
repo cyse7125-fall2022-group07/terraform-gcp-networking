a13a82fa49ed69fe4a3bf347d3892e6b974c9ca2
Shrawani <karpe.s@northeastern.edu>
2022-11-08 00:42:33 -0500

    external ip

--- networking/variables.tf
+++ networking/variables.tf
@@ -4,9 +4,6 @@ variable "region" {
 }
 variable "project_id" {
     type   = string
-    default = "assignment-2-28-92022"
-    # default = "vharsora-1701" #vinit
-    # default = "assignment-30-9-2022-602" #harshal
 }
 variable "instance_name"{
     type   = string
